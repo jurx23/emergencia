43d1bf455d42f4d862f6c3f6a0973ba89afdf5df
Adail Goncalves <juliano.a.goncalves@gmail.com>
2022-11-21 20:11:52 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ const doencas = document.getElementById('doencas');
 const tipoSanguineo = document.getElementById('tipoSanguineo');
 
 function calculaIdade() {
-    const dataNascimento = new Date(1993,2,23);
+    const dataNascimento = new Date();
     const dataAtual = new Date();
     
     let anos = dataAtual.getFullYear() - dataNascimento.getFullYear();
@@ -26,22 +26,22 @@ function calculaIdade() {
         }
     }
 
-    dtNascimento.innerText = '23/03/1993';
+    dtNascimento.innerText = '';
     idade.innerText = '- ' + anos + ' anos';
 }
 
 onload = () => {
 
-    sexo.innerText = 'Masculino';
+    sexo.innerText = '';
 
     calculaIdade();
 
-    residencia.innerText = 'São Bernardo do Campo - SP';
-    convenioMedico.innerText = 'Unimed';
-    carteirinha.innerText = '0 994 8628 50282400-7';
-    sus.innerText = '898002941323505';
-    alergias.innerText = 'Não';
-    doencas.innerText = 'Rinite Alérgica';
-    tipoSanguineo.innerText = 'O+';
+    residencia.innerText = '';
+    convenioMedico.innerText = '';
+    carteirinha.innerText = '';
+    sus.innerText = '';
+    alergias.innerText = '';
+    doencas.innerText = '';
+    tipoSanguineo.innerText = '';
 }
 
